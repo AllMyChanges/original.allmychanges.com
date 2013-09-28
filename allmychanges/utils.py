e3fa732f6f3362a109841d1cab8ca2e20d90f5c6
Alexander Artemenko <svetlyak.40wt@gmail.com>
2013-09-28 11:27:05 +0000

    Fixed  parsing for Flask, added command to fill database from commandline.

---
+++ allmychanges/utils.py
@@ -0,0 +1,33 @@
+import os
+import string
+
+from contextlib import contextmanager
+
+
+def load_data(filename):
+    data = []
+    with open(filename) as f:
+        for line in f.readlines():
+            data.append(
+                tuple(map(string.strip, line.split(';', 1))))
+
+    return data
+
+
+@contextmanager
+def cd(path):
+    """Usage:
+
+    with cd(to_some_dir):
+        envoy.run('task do')
+    """
+    old_path = os.getcwd()
+    os.chdir(path)
+    yield
+    os.chdir(old_path)
+
+
+
+def get_package_name(path):
+    with cd(path):
+        evoy
