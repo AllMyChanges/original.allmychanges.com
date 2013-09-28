9d12a37eb0612797e91bb13d645407febfa6ff40
Alexander Artemenko <svetlyak.40wt@gmail.com>
2013-09-28 03:26:15 +0000

    Added code to render changes in html.

--- allmychanges/views.py
+++ allmychanges/views.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 from vanilla import FormView
 from .forms import GetChangeLogForm
 from django.shortcuts import render
@@ -8,7 +10,33 @@ class IndexView(FormView):
     template_name = 'index.html'
 
     def form_valid(self, form):
+        changes =    [
+            {
+                'version': '0.1.3',
+                'sections': [
+                    {
+                        'items': [
+                            'Исправлен баг с кодировками',
+                            'Добавлена возможность шарить папки'
+                            ]
+                        }
+                    ]
+                },
+            {
+                'version': '0.1.2',
+                'sections': [
+                    {
+                        'items': [
+                            'Добавлен перевод на китайский язык',
+                            'Цвет шапки теперь более привлекательный — красный',
+                            'Наконец стали вести ChangeLog'
+                            ]
+                        }
+                    ]
+                }
+            ]
         return self.render_to_response(dict(
                 job_id=random.randint(0, 1000),
+                changes=changes,
                 form=form))
 
