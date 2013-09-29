21cb31a7a27329e8ef2f8e8082cf81f737f3d16e
Chibisov Gennady <web-chib@yandex-team.ru>
2013-09-29 12:41:16 +0400

    favicon

--- allmychanges/urls.py
+++ allmychanges/urls.py
@@ -5,12 +5,14 @@ admin.autodiscover()
 
 from .views import IndexView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.shortcuts import redirect
 
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^v1/', include('allmychanges.urls_api')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^favicon.ico/$', lambda x: redirect('/static/favicon.ico')),
 )
 
 urlpatterns += staticfiles_urlpatterns()
