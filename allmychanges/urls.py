f341a39fddb1f0e3585c5ab1b6417d7f30b0489a
Chibisov Gennady <web-chib@yandex-team.ru>
2013-09-29 21:17:43 +0400

    twitter and humans.txt

--- allmychanges/urls.py
+++ allmychanges/urls.py
@@ -3,13 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from .views import IndexView
+from .views import IndexView, HumansView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.shortcuts import redirect
 
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^humans.txt/$', HumansView.as_view(), name='humans'),
     url(r'^v1/', include('allmychanges.urls_api')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^favicon.ico/$', lambda x: redirect('/static/favicon.ico')),
